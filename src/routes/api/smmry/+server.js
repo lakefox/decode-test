0d02052fbd9f17737a065d2805683d95983d8808
LakeFox <mason@lakefox.net>
2022-10-15 23:36:06 -0500

    d

--- src/routes/api/smmry/+server.js
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
-import { summarizeFromUrl } from "./summary.mjs";
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET({ url }) {
-    // console.log(url);
-    let smmry = await summarizeFromUrl(decodeURIComponent(url.href.slice(url.href.indexOf("?") + 1)));
-    return new Response(smmry.split("\n").slice(1).join("\n"))
-}
